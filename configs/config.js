58410f183c5f8390254f167af0279ae2c2a3dfc8
Lê Huỳnh Hương <lehuynhhuong.0610@gmail.com>
2024-04-02 12:53:25 +0700

    nop bai

---
+++ configs/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    SECRET_KEY: 'lehuynhhuong',
+    EXPIRE_JWT: '1d',
+    hostName: 'localhost:3000',
+    Host: "sandbox.smtp.mailtrap.io",
+    Port: 25,
+    "Username" : "106795bdf97321",
+    "Password" : "9cfbbdd70b2864"
+}
